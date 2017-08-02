08c96472209634969de5913ac55d6bdc6359242c
Hector Garcia <hectorgarcia@gmail.com>
2017-08-02 16:39:26 +0900

    Top sra page

--- sra_download_all_samples.sh
+++ sra_download_all_samples.sh
@@ -1,3 +1,5 @@
+# Top: https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?study=ERP012803
+# 
 # https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=samples&term=sibo&go=Go
 # https://www.ncbi.nlm.nih.gov/sra/ERX2092801[accn]
 # https://www.ncbi.nlm.nih.gov/Traces/study/?acc=SAMEA104163721
