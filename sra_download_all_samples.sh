4153d77e1e913a5fe87714d6f2b5048e6af580ce
Hector Garcia <hectorgarcia@gmail.com>
2017-08-02 16:34:18 +0900

    Sra download all samples

---
+++ sra_download_all_samples.sh
@@ -0,0 +1,5 @@
+# https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=samples&term=sibo&go=Go
+# https://www.ncbi.nlm.nih.gov/sra/ERX2092801[accn]
+# https://www.ncbi.nlm.nih.gov/Traces/study/?acc=SAMEA104163721
+
+wget 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&rettype=runinfo&db=sra&term==PRJEB11419' -O - | tee SraRunInfo.csv
