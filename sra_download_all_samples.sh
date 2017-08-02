3eb90cedaa8c5ac7de5a0787ca2188b4c777d7a5
Hector Garcia <hectorgarcia@gmail.com>
2017-08-02 16:44:05 +0900

    sibo sample added

--- sra_download_all_samples.sh
+++ sra_download_all_samples.sh
@@ -3,5 +3,8 @@
 # https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?view=samples&term=sibo&go=Go
 # https://www.ncbi.nlm.nih.gov/sra/ERX2092801[accn]
 # https://www.ncbi.nlm.nih.gov/Traces/study/?acc=SAMEA104163721
+#
+# Sibo sample: https://www.ncbi.nlm.nih.gov/biosample/SAMEA104162366
+#
 
 wget 'http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&rettype=runinfo&db=sra&term==PRJEB11419' -O - | tee SraRunInfo.csv
